be7f38ffe6bd524e09217c194939280058eac9db
spencer kelly <spencermountain@gmail.com>
2019-01-23 17:27:44 -0500

    0.1.0

--- src/suffix.js
+++ src/suffix.js
@@ -1,5 +1,9 @@
 //append text to the filename
 function addSuffix(filename, string) {
+  if (!string) {
+    return filename
+  }
+  string = '-' + string
   var dotIndex = filename.lastIndexOf(".");
   if (dotIndex === -1) return filename + string;
   else return filename.substring(0, dotIndex) + string + filename.substring(dotIndex);
